90b1631a5d0f6b7eaa22b1259f63a594265675d3
Grip <144571646+x0d167@users.noreply.github.com>
2026-07-31 14:27:29 -0500

    Update git.nix

--- home/modules/git.nix
+++ home/modules/git.nix
@@ -5,7 +5,7 @@
     settings = {
       user = {
         name = "x0d167";
-        email = "x0d1.67@proton.me";
+        email = "144571646+x0d167@users.noreply.github.com";
         signingkey = "1A6A98C5E6F6ACE952F77995FD3DA856728F87D6"; # key2 Sign subkey
       };
       commit.gpgsign = true;
